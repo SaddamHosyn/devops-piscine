92d5846249c3eba344dd57586c9b2b896a6edbd3
shussain <saddamhussain1919@gmail.com>
2026-02-17 13:59:53 +0000

    updated

--- head-and-tail.sh
+++ head-and-tail.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-curl -s https://assets.01-edu.org/devops-branch/HeadTail.txt | head -1 && curl -s https://assets.01-edu.org/devops-branch/HeadTail.txt | tail -
-1
+curl -s https://assets.01-edu.org/devops-branch/HeadTail.txt | head -1 && curl -s https://assets.01-edu.org/devops-branch/HeadTail.txt | tail -1
