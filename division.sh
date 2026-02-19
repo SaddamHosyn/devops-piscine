fe347001ee2db9f4c93672bda6ab05ae36dbcd55
shussain <saddamhussain1919@gmail.com>
2026-02-19 19:44:12 +0000

    added

--- division.sh
+++ division.sh
@@ -1,20 +1,14 @@
 #!/usr/bin/env bash
-# Check if two arguments were provided
-if [ $# -ne 2 ]; then
-	echo "Error: two numbers must be provided"
 
-# # Check if the arguments are numeric
-elif ! [[ $1 =~ ^-?[0-9]*\.?[0-9]+$ ]] || ! [[ $2 =~ ^-?[0-9]*\.?[0-9]+$ ]]; then
-	echo "Error: both arguments must be numeric"
+if [[ $# -ne 2 ]]; then
+    echo "Error: two numbers must be provided"
 
-# Check if the second argument is not 0
-elif [ $(echo "$2 == 0" | bc) -eq 1 ]; then
-	echo "Error: division by zero is not allowed"
+elif ! [[ $1 =~ ^-?[0-9]+$ ]] || ! [[ $2 =~ ^-?[0-9]+$ ]]; then
+    echo "Error: both arguments must be integers"
 
-# Divide the first argument by the second using bc
+elif [[ $(echo "$2 == 0" | bc) -eq 1 ]]; then
+    echo "Error: division by zero is not allowed"
+
 else
-	result=$(echo "$1 / $2" | bc)
-
-	# Output the result
-	echo $result
+    echo "$1 / $2" | bc
 fi
