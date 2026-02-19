a398ae6d4829fbdb0d058cf25db9be508d4c9e95
shussain <saddamhussain1919@gmail.com>
2026-02-19 19:37:39 +0000

    added

--- division.sh
+++ division.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 if (( $# < 2 )); then
-    echo "Error: two numbers must be provided."
+    echo "Error: two numbers must be provided"
     exit 1
 fi
 
 if ! echo "$1" | grep -qE '^-?[0-9]+$' || ! echo "$2" | grep -qE '^-?[0-9]+$'; then
-    echo "Error: both arguments must be integers."
+    echo "Error: both arguments must be integers"
     exit 1
 fi
 
 if echo "$2" | grep -qE '^-?0+$'; then
-    echo "Error: division by zero is not allowed."
+    echo "Error: division by zero is not allowed"
     exit 1
 fi
 
