bd01e7dc615ff327b318de1eccdc6ecb789f48a0
shussain <saddamhussain1919@gmail.com>
2026-02-20 22:30:30 +0000

    added

--- array-selector.sh
+++ array-selector.sh
@@ -2,14 +2,14 @@
 
 if [[ $# -ne 1 ]]; then
     echo "Error"
-
+    exit 1
 fi
 if [[ ! $1 =~ ^[0-9]+$ ]]; then
- 
+    echo "Error"
+    exit 1
 fi
 if [[ $1 -lt 1 ]] || [[ $1 -gt 5 ]]; then
     echo "Error"
-
 fi
 array=("red" "blue" "green" "white" "black")
 echo ${array[$(( $1 - 1 ))]}
