132d6bee5b944316f695e8f15678979abc0b072e
shussain <saddamhussain1919@gmail.com>
2026-02-20 22:17:43 +0000

    added

--- array-selector.sh
+++ array-selector.sh
@@ -4,18 +4,12 @@ if [[ $# -ne 1 ]]; then
     echo "Error"
     exit 1
 fi
-
 if [[ ! $1 =~ ^[0-9]+$ ]]; then
     echo "Error"
     exit 1
 fi
-
 if [[ $1 -lt 1 ]] || [[ $1 -gt ${#array[@]} ]]; then
     echo "Error"
     exit 1
 fi
-
 echo ${array[$(( $1 - 1 ))]}
-
-
-
