b0146a632fe407591f13315e1e433eca622375de
shussain <saddamhussain1919@gmail.com>
2026-02-21 00:58:12 +0000

    added

--- comparator.sh
+++ comparator.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 if [[ $# -lt 2 ]]; then
     echo "Error: The script only works with two arguments!"
-    exit 1
+  
 fi
 if [[ $# -gt 2 ]]; then
     echo "Error: The script only works with two arguments!"
-    exit 1
+ 
 fi
 if [[ ! $1 =~ ^[0-9]+$ ]] || [[ ! $2 =~ ^[0-9]+$ ]]; then
     echo "Error: Only two numeric arguments are acceptable!"
-    exit 1
+   
 fi
 if  [[ $1 -eq $2 ]]; then
     echo "$1 = $2"
