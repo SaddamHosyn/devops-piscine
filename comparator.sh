4118d0f15c1d69f361d9beb149e68ab7d79ca514
shussain <saddamhussain1919@gmail.com>
2026-02-21 01:01:46 +0000

    added

--- comparator.sh
+++ comparator.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
-if [[ $# -lt 2 ]]; then
+if [[ $# -ne 2 ]]; then
     echo "Error: The script only works with two arguments!"
-  
-fi
-if [[ $# -gt 2 ]]; then
-    echo "Error: The script only works with two arguments!"
- 
-fi
-if [[ ! $1 =~ ^[0-9]+$ ]] || [[ ! $2 =~ ^[0-9]+$ ]]; then
+elif [[ ! $1 =~ ^[0-9]+$ ]] || [[ ! $2 =~ ^[0-9]+$ ]]; then
     echo "Error: Only two numeric arguments are acceptable!"
-   
-fi
-if  [[ $1 -eq $2 ]]; then
+elif [[ $1 -eq $2 ]]; then
     echo "$1 = $2"
-fi
-if  [[ $1 -lt $2 ]]; then
+elif [[ $1 -lt $2 ]]; then
     echo "$1 < $2"
-fi
-if  [[ $1 -gt $2 ]]; then
+elif [[ $1 -gt $2 ]]; then
     echo "$1 > $2"
 fi
-
