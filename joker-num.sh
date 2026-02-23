2845731439e1900b118bfcd07e7b5015c724197c
shussain <saddamhussain1919@gmail.com>
2026-02-23 14:08:47 +0000

    added

--- joker-num.sh
+++ joker-num.sh
@@ -1,49 +1,43 @@
 #!/bin/bash
 
-#######################################
-# 1. Validate arguments
-#######################################
-
-if [[ $# -ne 1 || -z "$1" || ! "$1" =~ ^[0-9]+$ || "$1" -lt 1 || "$1" -gt 100 ]]; then
-    echo "Error: wrong argument"
-    exit 1
+if [[ $# != 1 ||
+	-z "$1" ||
+	! "$1" =~ ^[0-9]+$ ||
+	"$1" -lt 1 ||
+	"$1" -gt 100 ]]; then
+	echo "Error: wrong argument"
+	exit 1
 fi
 
-#######################################
-# 2. Store the secret number
-#######################################
+number=$1
 
-secret="$1"
+# Start the for loop for player two
+for ((tries_left = 5; tries_left > 0; tries_left--)); do
+	echo "Enter your guess ($tries_left tries left):"
 
-#######################################
-# 3. Game loop (5 tries) using for
-#######################################
+	read guess
+	if [[ $? -lt 0 ]]; then
+		exit 1
+	fi
 
-valid_tries=0
-for i in {1..5}; do
-    tries_left=$((5 - valid_tries))
-    echo "Enter your guess (${tries_left} tries left):"
-    read guess
-    
-    # 3.1 validate guess (empty / non-numeric / out of range)
-    if [[ -z "$guess" || ! "$guess" =~ ^[0-9]+$ || "$guess" -lt 1 || "$guess" -gt 100 ]]; then
-        continue  # retry without counting as valid try
-    fi
-    
-    # 3.2 valid guess: count it and compare
-    valid_tries=$((valid_tries + 1))
-    if [[ "$guess" -gt "$secret" ]]; then
-        echo "Go down"
-    elif [[ "$guess" -lt "$secret" ]]; then
-        echo "Go up"
-    else
-        echo "Congratulations, you found the number in ${valid_tries} moves!"
-        exit 0
-    fi
+	if [[ -z "$guess" ||
+		! "$guess" =~ ^[0-9]+$ ||
+		"$guess" -lt 1 ||
+		"$guess" -gt 100 ]]; then
+		tries_left=$tries_left+1
+		continue
+	fi
+
+	if [[ "$guess" -eq "$number" ]]; then
+		echo "Congratulations, you found the number in $((5 - $tries_left + 1)) moves!"
+		exit
+	fi
+
+	if [[ "$guess" -lt "$number" ]]; then
+		echo "Go up"
+	else
+		echo "Go down"
+	fi
 done
 
-#######################################
-# 4. If we reach here, player lost
-#######################################
-
-echo "You lost, the number was $secret"
+echo "You lost, the number was $number"
